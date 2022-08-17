e12829de8300887a1aa5deced3da7dcfb53540c9
Ali Ahmad <user@Alis-MBP.fios-router.home>
2022-08-17 18:54:04 -0400

    playing validator errors

--- val/validator.go
+++ val/validator.go
@@ -22,11 +22,11 @@ func validateString(value string, minLength int, maxLengeth int) error {
 func ValidateUsername(value string) error {
 	err := validateString(value, 3, 100)
 	if err != nil {
-		return fmt.Errorf("username: %s cannot be blank and must be between 3-100 characters", value, err)
+		return fmt.Errorf("username: %s cannot be blank and must be between 3-100 characters", err)
 	}
 
 	if !isValidUsername(value) {
-		return fmt.Errorf("Username must contain letters, numbers or underscore")
+		return fmt.Errorf("username must contain letters, numbers or underscore")
 	}
 
 	return nil
@@ -35,11 +35,11 @@ func ValidateUsername(value string) error {
 func ValidateFullName(value string) error {
 	err := validateString(value, 3, 100)
 	if err != nil {
-		return fmt.Errorf("Fullname: %s cannot be blank and must be between 3-100 characters", value, err)
+		return fmt.Errorf("fullname: %s cannot be blank and must be between 3-100 characters", err)
 	}
 
 	if !isValidFullName(value) {
-		return fmt.Errorf("Name must contain letters and must be between 3 and 100 characters")
+		return fmt.Errorf("name must contain letters and must be between 3 and 100 characters")
 	}
 
 	return nil
@@ -48,9 +48,9 @@ func ValidateFullName(value string) error {
 func ValidatePassword(value string) error {
 	err := validateString(value, 6, 100)
 	if err != nil {
-		return fmt.Errorf("Password cannot be blank and must be between 6-100 characters", err)
+		return fmt.Errorf("password cannot be blank and must be between 6-100 characters")
 	}
-	return err
+	return nil
 }
 
 func ValidateEmail(value string) error {
@@ -59,8 +59,8 @@ func ValidateEmail(value string) error {
 		return err
 	}
 	_, err = mail.ParseAddress(value)
-	if err != nil{
+	if err != nil {
 		return fmt.Errorf("%s is not a value email address", value)
 	}
-	return err
+	return nil
 }
